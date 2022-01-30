8d1bedd92062c95c2a01923ee55b9b05a6279af3
Dmitry Vkhv <v.dmitry.v@gmail.com>
2022-01-30 18:14:53 +0500

    сделал как мог 4 задание

--- Vakhov_Dmitry_dz_3/task_3_4.py
+++ Vakhov_Dmitry_dz_3/task_3_4.py
@@ -1,21 +1,21 @@
-from task_3_3 import thesaurus
-
+# from task_3_3 import thesaurus
 
 def thesaurus_adv(*args) -> dict:
     # пишите свою реализацию здесь
     dict_out = {}  # результирующий словарь значений
+    my_list: list
     for i in args:
-        i: tuple
-        b = list(i)
-        c = b[0]
-        # dict_out[c] = args[i]
+        my_list=i
+        c = my_list[my_list.find(' ')+1]
         if c not in dict_out.keys():
-            dict_out.update({c: [i]})
+            dict_out.update({c: [my_list]})
         else:
             dict_out[c].append(i)
     return dict_out
 
 
-print(thesaurus_adv("Иван Сергеев", "Инна Серова", "Петр Алексеев", "Илья Иванов", "Анна Савельева"))
+print(thesaurus_adv("Иван Сергеев", "Инна Серова", "Петр Алексеев", "Илья Иванов", "Анна Савельева",
+                           "Антон Ильин"))
+
 
 # TODO: Разобраться как сортировать по фамилиям, и как отсортировать по ключу.
