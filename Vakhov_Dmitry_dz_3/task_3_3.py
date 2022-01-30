3a8e951e15ccbbdac83d1d542ec08679a634423a
Dmitry Vkhv <v.dmitry.v@gmail.com>
2022-01-30 20:23:29 +0500

    решение задач третьего урока - второй вариант

---
+++ Vakhov_Dmitry_dz_3/task_3_3.py
@@ -0,0 +1,22 @@
+def thesaurus(*args) -> dict:
+    # пишите свою реализацию здесь
+    dict_out = {}  # результирующий словарь значений
+    for i in args:
+        # i: tuple
+        # b = list(i)
+        # c = b[0]
+        c = list(i)[0]
+        # dict_out[c] = args[i]
+        if c not in dict_out.keys():
+            dict_out.update({c: [i]})
+        else:
+            dict_out[c].append(i)
+    # print(type(dict_out))
+    # print(type(dict_out['И']))
+    # dict_out = sorted(dict_out.items())
+    return dict_out
+    # return sorted(dict_out.items())
+
+
+print(thesaurus("Иван", "Мария", "Петр", "Илья", "Анатолий"))
+print(dict(sorted(thesaurus("Иван", "Мария", "Петр", "Илья", "Анатолий").items())))
