5e9d98807e3488425048a768b88d5bff5560cf98
Dmitry Vkhv <v.dmitry.v@gmail.com>
2022-01-30 18:54:57 +0500

    решение 5 задачи

--- Vakhov_Dmitry_dz_3/task_3_5.py
+++ Vakhov_Dmitry_dz_3/task_3_5.py
@@ -5,27 +5,21 @@ adverbs = ["сегодня", "вчера", "завтра", "позавчера",
 adjectives = ["веселый", "яркий", "зеленый", "утопичный", "мягкий"]
 
 
-def get_jokes(count: int, can_repeat=False, **kwargs) -> list:
+def get_jokes(count: int) -> list:
     """Возвращает список шуток в количестве count"""
     # пишите реализацию своей программы здесь
     max_count = min(len(nouns), len(adverbs), len(adjectives))
-    count = int(input(f'Сколько шуток хотите получить? Но не более {max_count} '))
+    count = int(input(f'Сколько шуток хотите получить? Но не более {max_count} =) '))
     if count > max_count:
         count = max_count
     list_out = []
 
     for _ in range(count):
-        if can_repeat:
-            list_out.append(' '.join(random.choice(kwargs[j]) for j in kwargs.keys()))
-        else:
-            noun, adverb, adjective = [random.choice(kwargs[j]) for j in kwargs.keys()]
-            list_out.append(' '.join([noun, adverb, adjective]))
-            kwargs['nouns'].remove(noun)
-            kwargs['adverbs'].remove(adverb)
-            kwargs['adjectives'].remove(adjective)
-
+        i = random.choice(nouns)
+        j = random.choice(adverbs)
+        k = random.choice(adjectives)
+        list_out.append(' '.join([i, j, k]))
     return list_out
 
 
-# print(get_jokes(2, nouns=nouns, adverbs=adverbs, adjectives=adjectives))
-print(get_jokes(10, nouns=nouns, adverbs=adverbs, adjectives=adjectives))
+print(get_jokes(10))
