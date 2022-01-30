b00a509de6e3159b20c9afb94519ef8947eb3353
Dmitry Vkhv <v.dmitry.v@gmail.com>
2022-01-30 13:36:55 +0500

    поправил з решение

--- Vakhov_Dmitry_dz_3/task_3_3.py
+++ Vakhov_Dmitry_dz_3/task_3_3.py
@@ -2,9 +2,10 @@ def thesaurus(*args) -> dict:
     # пишите свою реализацию здесь
     dict_out = {}  # результирующий словарь значений
     for i in args:
-        i: tuple
-        b = list(i)
-        c = b[0]
+        # i: tuple
+        # b = list(i)
+        # c = b[0]
+        c = list(i)[0]
         # dict_out[c] = args[i]
         if c not in dict_out.keys():
             dict_out.update({c: [i]})
@@ -12,7 +13,10 @@ def thesaurus(*args) -> dict:
             dict_out[c].append(i)
     # print(type(dict_out))
     # print(type(dict_out['И']))
+    # dict_out = sorted(dict_out.items())
     return dict_out
+    # return sorted(dict_out.items())
 
 
-print(thesaurus("Иван", "Мария", "Петр", "Илья"))
+print(thesaurus("Иван", "Мария", "Петр", "Илья", "Анатолий"))
+print(sorted(thesaurus("Иван", "Мария", "Петр", "Илья", "Анатолий").items()))
